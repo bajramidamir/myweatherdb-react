717656b64cebecb28cd15287feca8fb1b84b8570
Tempest20 <bajrami.damir@gmail.com>
2022-12-08 20:31:04 +0100

    removed whitespaces

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,6 @@ import React from 'react';
 const Footer = () => {
   return (
     <>
-    
         <footer className='bg-gray-200 dark:bg-stone-800  text-center lg:text-left fixed bottom-0 w-full'>
             <div className='text-gray-700 text-center dark:text-white p-4'>
             © 2022 Copyright:&nbsp; 
@@ -13,9 +12,9 @@ const Footer = () => {
             underline-offset-2 " target="_blank">Damir Bajrami</a>
             </div>
         </footer>
-    
     </>
   )
 }
+
 
 export default Footer;
