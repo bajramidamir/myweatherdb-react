55bdb6427413eda631d2b4351a49b7b340b4ec2c
Tempest20 <bajrami.damir@gmail.com>
2022-12-24 18:54:43 +0100

    changed urls

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,7 +6,7 @@ const Footer = () => {
         <footer className='bg-gray-200 dark:bg-stone-800  text-center lg:text-left fixed bottom-0 w-full'>
             <div className='text-gray-700 text-center dark:text-white p-4'>
             © 2022 Copyright:&nbsp; 
-            <a href="https://www.github.com/Tempest20" className="hover:underline 
+            <a href="https://github.com/bajramidamir" className="hover:underline 
             decoration-red-500 
             dark:decoration-emerald-300 
             underline-offset-2 " target="_blank">Damir Bajrami</a>
